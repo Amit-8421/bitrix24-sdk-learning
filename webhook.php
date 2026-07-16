9947c564aed83394f05f7849cad5a6ad51fe9b0d
Amit Janekar <amitjankear30@gmail.com>
2026-07-16 23:18:12 +0530

    Add webhook

--- webhook.php
+++ webhook.php
@@ -5,13 +5,12 @@ date_default_timezone_set('Asia/Kolkata');
 $log = [
     'time' => date('Y-m-d H:i:s'),
     'post' => $_POST,
-    'get'  => $_GET,
+    'get' => $_GET,
 ];
 
 file_put_contents(
     __DIR__ . '/log.txt',
-    print_r($log, true) . PHP_EOL .
-    str_repeat('-', 80) . PHP_EOL,
+    print_r($log, true),
     FILE_APPEND
 );
 
