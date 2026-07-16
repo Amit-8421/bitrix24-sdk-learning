ae1af3ec65e5aba5c4db12a9a32b760beb5d7db2
Amit Janekar <amitjankear30@gmail.com>
2026-07-16 23:50:46 +0530

    update

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'bitrix_webhook' => 'https://cis-official.bitrix24.in/rest/98/lqx1r6iyi55dp0i4',
+    'bitrix_webhook' => 'https://cis-official.bitrix24.in/rest/98/lqx1r6iyi55dp0i4/',
 ];
--- webhook.php
+++ webhook.php
@@ -1,9 +1,24 @@
 <?php
 
+$config = require 'config.php';
 
-file_put_contents(
-    'php://stderr',
-    "WEBHOOK HIT\n"
-);
+$webhook = rtrim($config['bitrix_webhook'], '/') . '/';
 
-echo "OK";
+$data = [
+    'fields' => [
+        'TITLE' => 'Webhook Test Deal - ' . date('Y-m-d H:i:s')
+    ]
+];
+
+$ch = curl_init($webhook . 'crm.deal.add.json');
+
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => http_build_query($data),
+]);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
