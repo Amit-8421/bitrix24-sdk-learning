90c0afb23e2e19e7aff3e572ba7a08fb62f7167c
Amit Janekar <amitjankear30@gmail.com>
2026-07-16 23:57:56 +0530

    Add webhook

--- webhook.php
+++ webhook.php
@@ -1,24 +1,3 @@
 <?php
 
-$config = require 'config.php';
-
-$webhook = rtrim($config['bitrix_webhook'], '/') . '/';
-
-$data = [
-    'fields' => [
-        'TITLE' => 'Webhook Test Deal - ' . date('Y-m-d H:i:s')
-    ]
-];
-
-$ch = curl_init($webhook . 'crm.contact.add.json');
-
-curl_setopt_array($ch, [
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_POST => true,
-    CURLOPT_POSTFIELDS => http_build_query($data),
-]);
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-echo $response;
+die("WEBHOOK HIT");
