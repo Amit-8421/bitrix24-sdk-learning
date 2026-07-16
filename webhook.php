43efb8bd8d73f0b42e13f2479e6ff67561fdc88b
Amit Janekar <amitjankear30@gmail.com>
2026-07-16 23:44:02 +0530

    Add webhook

--- webhook.php
+++ webhook.php
@@ -1,8 +1,9 @@
 <?php
 
-error_log("===== WEBHOOK HIT =====");
-error_log("Event: " . ($_REQUEST['event'] ?? ''));
-error_log("Token: " . ($_REQUEST['auth']['application_token'] ?? ''));
-error_log(print_r($_REQUEST, true));
+
+file_put_contents(
+    'php://stderr',
+    "WEBHOOK HIT\n"
+);
 
 echo "OK";
