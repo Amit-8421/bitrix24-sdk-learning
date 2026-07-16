ca2cbd44af12b7ae40207cfbabc537daed0a2dcf
Amit Janekar <amitjankear30@gmail.com>
2026-07-17 00:05:58 +0530

    Add webhook

--- webhook.php
+++ webhook.php
@@ -1,9 +1,11 @@
 <?php
 
-header('Content-Type: application/json');
+http_response_code(200);
 
-echo json_encode([
-    'request' => $_REQUEST,
-    'post'    => $_POST,
-    'get'     => $_GET,
-]);
+file_put_contents(
+    __DIR__ . '/request.txt',
+    print_r($_REQUEST, true),
+    FILE_APPEND
+);
+
+echo "OK";
