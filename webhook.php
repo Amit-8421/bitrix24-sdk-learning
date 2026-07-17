9aeafb466c8615e5acd32db06f81b120f7304dfd
Amit Janekar <amitjankear30@gmail.com>
2026-07-17 10:19:04 +0530

    Add webhook

--- webhook.php
+++ webhook.php
@@ -1,9 +1,16 @@
 <?php
 
+$data = [
+    'time' => date('Y-m-d H:i:s'),
+    'method' => $_SERVER['REQUEST_METHOD'],
+    'post' => $_POST,
+    'raw' => file_get_contents('php://input'),
+];
+
 file_put_contents(
-    __DIR__ . "/hit.txt",
-    "Webhook Hit - " . date("Y-m-d H:i:s") . PHP_EOL,
+    __DIR__ . '/request.txt',
+    print_r($data, true) . PHP_EOL . str_repeat('-', 80) . PHP_EOL,
     FILE_APPEND
 );
 
-echo "OK2";
+echo "OK3";
