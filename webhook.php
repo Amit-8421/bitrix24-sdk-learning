c1edc6b84face00acf561a13d3dd5ec319b3e5f1
Amit Janekar <amitjankear30@gmail.com>
2026-07-17 10:31:29 +0530

    Add webhook

--- webhook.php
+++ webhook.php
@@ -1,17 +1,28 @@
 <?php
 
-$data = [
-    'time' => date('Y-m-d H:i:s'),
-    'method' => $_SERVER['REQUEST_METHOD'],
-    'post' => $_POST,
-    'raw' => file_get_contents('php://input'),
-];
+declare(strict_types=1);
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Bitrix24\SDK\Services\ServiceBuilderFactory;
+
+$config = require 'config.php';
+
+$leadId = (int)$_POST['data']['FIELDS']['ID'];
+
+$b24 = ServiceBuilderFactory::createServiceBuilderFromWebhook(
+    $config['bitrix_webhook']
+);
+
+$response = $b24->core->call('crm.lead.get', [
+    'id' => $leadId
+]);
+
+$lead = $response->getResponseData()->getResult();
 
 file_put_contents(
-    __DIR__ . '/request.txt',
-    print_r($data, true) . PHP_EOL .
-    str_repeat('-', 80) . PHP_EOL,
-    FILE_APPEND
+    __DIR__ . '/lead.txt',
+    print_r($lead, true)
 );
 
 echo "OK";
